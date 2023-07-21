a6bf301b36f31b3250582397d5455fadd4397c89
p4p1 <p4p1@vivaldi.net>
2023-07-21 20:09:01 +0200

    please god save my work from eternal doom

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setup.sh
+# Created on: Fri 21 Jul 2023 04:19:21 PM CEST
+#
+#  ____   __  ____  __
+# (  _ \ /. |(  _ \/  )
+#  )___/(_  _))___/ )(
+# (__)    (_)(__)  (__)
+#
+# Description:
+#
+
+
+mkdir repo
+repo-add -n repo/p3ng0s.db.tar.gz $(find . -name "*.tar.zst")
