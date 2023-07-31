7a93e09ab55573b6e0d32cdf6e01a0215fc00d72
p4p1 <p4p1@vivaldi.net>
2023-07-31 23:29:48 +0200

    removed i3lock-fancy fork

--- setup.sh
+++ setup.sh
@@ -18,7 +18,8 @@ AUR=("https://aur.archlinux.org/neo4j-community.git" "https://aur.archlinux.org/
 	"https://aur.archlinux.org/python-pypykatz.git" "https://aur.archlinux.org/python-winacl.git" \
 	"https://aur.archlinux.org/python-winsspi.git" "https://aur.archlinux.org/python-aiosmb.git" \
 	"https://aur.archlinux.org/python-aiowinreg.git" "https://aur.archlinux.org/python-aesedb.git" \
-	"https://aur.archlinux.org/python-unicrypto.git")
+	"https://aur.archlinux.org/python-unicrypto.git" "https://aur.archlinux.org/python-asyauth.git" \
+	"https://aur.archlinux.org/i3lock-fancy-git.git" "https://aur.archlinux.org/emojify.git")
 #	"https://aur.archlinux.org/ruby-evil-winrm.git" "https://aur.archlinux.org/ruby-winrm.git" "https://aur.archlinux.org/ruby-winrm-fs.git")
 
 for u in ${AUR[*]}; do
